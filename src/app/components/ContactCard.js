c8034827cc3bf1b4a4b9b565567559594c48b229
sandeep pal <sandeeppal7208@gmail.com>
2023-04-28 12:48:27 +0530

    contact-card

--- src/app/components/ContactCard.js
+++ src/app/components/ContactCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "@/app/contact/contact.module.css"
+import styles from "@/app/Contact/contact.module.css"
 import { MdEmail,MdVoiceChat,MdForum } from 'react-icons/md';
 import Link from 'next/link';
 
